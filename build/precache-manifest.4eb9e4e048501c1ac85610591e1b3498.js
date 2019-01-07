a365f6434ef901fdce9e29d0a2932792037c4fdb
lihqi <455711093@qq.com>
2019-01-07 23:26:51 +0800

    修改readme

---
+++ build/precache-manifest.4eb9e4e048501c1ac85610591e1b3498.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/scroll-demo/build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4819f6cca9b2047d1477",
+    "url": "/scroll-demo/build/static/js/runtime~main.4819f6cc.js"
+  },
+  {
+    "revision": "066cb429d62f863f553e",
+    "url": "/scroll-demo/build/static/js/main.066cb429.chunk.js"
+  },
+  {
+    "revision": "bcf5f845f92bd59fa6f5",
+    "url": "/scroll-demo/build/static/js/1.bcf5f845.chunk.js"
+  },
+  {
+    "revision": "066cb429d62f863f553e",
+    "url": "/scroll-demo/build/static/css/main.a7fda24d.chunk.css"
+  },
+  {
+    "revision": "99ea047ce82bcb20828953e1e5260853",
+    "url": "/scroll-demo/build/index.html"
+  }
+];
